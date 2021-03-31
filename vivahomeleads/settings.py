10fb2ce65a13ec34c70e99f239a631b659d004e1
James Beegen <jamesbeegen@gmail.com>
2021-03-30 22:50:07 -0400

    database

--- vivahomeleads/settings.py
+++ vivahomeleads/settings.py
@@ -89,14 +89,15 @@ WSGI_APPLICATION = 'vivahomeleads.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
+DATABASES = DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
